1eb1e0aa0997d0cb9d09edead58dcf914c392d53
Svyatoslav <59284570+mutedalien@users.noreply.github.com>
2020-07-31 11:11:14 +0300

    less_1

--- mutedalien/routes/web.php
+++ mutedalien/routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +12,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
 });
