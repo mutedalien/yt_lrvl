9fa5f7b9cd335c8b48136b0793cd95a764231730
Svyatoslav <chel_c@mail.ru>
2020-08-01 18:21:49 +0300

    less_3

--- mutedalien/resources/views/contact.blade.php
+++ mutedalien/resources/views/contact.blade.php
@@ -4,4 +4,32 @@
 
 @section('content')
     <h1>Страница контактов</h1>
+
+    <form action="{{ route('contact-form') }}" method="post">
+    @csrf
+
+        <div class="form-group">
+            <label for="name">Введите имя</label>
+            <input type="text" name="name" placeholder="Введите имя" id="name" class="form-control">
+        </div>
+
+        <div class="form-group">
+            <label for="Email">Введите имя</label>
+            <input type="text" name="Email" placeholder="Введите Email" id="Email" class="form-control">
+        </div>
+
+        <div class="form-group">
+            <label for="subject">Тема сообщения</label>
+            <input type="text" name="subject" placeholder="Тема сообщения" id="subject" class="form-control">
+        </div>
+
+        <div class="form-group">
+            <label for="message">Сообщение</label>
+            <textarea name="message" id="message" placeholder="Введите сообщение" class="form-control"></textarea>
+        </div>
+
+    <button type="submit" class="btn btn-success">Отправить</button>
+
+    </form>
+
 @endsection
--- mutedalien/routes/web.php
+++ mutedalien/routes/web.php
@@ -13,12 +13,16 @@
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 
 Route::get('/about', function () {
     return view('about');
-});
+})->name('about');
 
 Route::get('/contact', function () {
     return view('contact');
-});
+})->name('contact');
+
+Route::post('/contact/submit', function () {
+    dd(Request::all());
+})->name('contact-form');
